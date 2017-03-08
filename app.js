5f774232ec8ac45a2b7542d997f944ed9a275913
netzrot <thorstenwschroeder@gmail.com>
2017-03-07 21:14:55 -0500

    add search functionality for ap

--- app.js
+++ app.js
@@ -59,58 +59,66 @@ app.post('/search', function(req, res) {
 
 	results = {
 		left: [],
-		center: ["Associated Press" + " " + newSearch],
-		
+		center: [],
 	};
 	
 
 	requesttool('http://www.huffingtonpost.com/search?keywords='+ newSearch + '&sortBy=recency&sortOrder=desc', function (error, response, html) {
 			
-			if (!error && response.statusCode == 200) {
-			    var $ = cheerio.load(html);
-			    
-			    $('a.card__link').each(function(i, element){
-			    	
-			      	var a = $(this); //gives link
-			      	var text = a.text(); // gives text in link
-			      	var url = a.attr('href'); // gives url
+		if (!error && response.statusCode == 200) {
+		    var $ = cheerio.load(html);
+		    
+		    $('a.card__link').each(function(i, element){
+		    	
+		      	var a = $(this); //gives link
+		      	var text = a.text(); // gives text in link
+		      	var url = a.attr('href'); // gives url
 
-			     	story = {
-			    		headline: text,
-			    		link: url
-			    	};
-			    	
-			    	results.left.push(story);
-		     	
-			    });				    
-			};
+		     	story = {
+		    		headline: text,
+		    		link: url
+		    	};
+		    	
+		    	results.left.push(story);
+	     	
+		    });				    
+		};
 
-			requesttool('http://api.foxnews.com/v1/content/search?q=' + newSearch + '&fields=description,title,url,image,type,taxonomy&sort=latest&section.path=fnc&type=article&start=0&callback=angular.callbacks._0&cb=201735140', function (error, response, html) {
-			
+			requesttool('http://hosted.ap.org/dynamic/external/search.hosted.ap.org/wireCoreTool/Search?SITE=AP&SECTION=HOME&TEMPLATE=DEFAULT&query=' + newSearch, function (error, response, html) {
+		
 					if (!error && response.statusCode == 200) {
-						var body = response.body.slice(21, response.body.length-1);
+						var $ = cheerio.load(response.body);
+		    
+				     	$('span.latestnews > a').each(function(i, element){
+		    	
+				    		var a = $(this); //gives link
+				    		var text = a.text(); // gives text in link
+		 		   			var url = a.attr('href'); // gives url
 
-						var bodyObj = JSON.parse(body);
+			 		    story = {
+				  			headline: text,
+				  			link: url
+			  		  	}
 
-						//console.log(bodyObj.response.docs);
-						results.right = bodyObj.response.docs;
-						console.log(results.right);
-					    
-					 // 	var $ = cheerio.load(html);
+			  		  	results.center.push(story);
 
-					 // 	$('a.ng-binding').each(function(i, element){	
+		  			    });
+	     	
+		    		};			
+	    
+				
 
-					 //    	var a = $(this); //gives link
-					 //    	var text = a.text(); // gives text in link
-				 	// 		console.log(text);  
-						// });
-					};
-
-				// console.log(results);
-				// console.log(newSearch);
-				res.render("results", { results });
+					requesttool('http://api.foxnews.com/v1/content/search?q=' + newSearch + '&fields=description,title,url,image,type,taxonomy&sort=latest&section.path=fnc&type=article&start=0&callback=angular.callbacks._0&cb=201735140', function (error, response, html) {
+					
+						if (!error && response.statusCode == 200) {
+							var body = response.body.slice(21, response.body.length-1);
+							var bodyObj = JSON.parse(body);
+								
+							results.right = bodyObj.response.docs;
+							res.render("results", { results });							    
+						};
+					});
 			});
-
 	});
 });
 
