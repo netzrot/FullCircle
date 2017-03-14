e4ae51655cdffdb6f6aa9ed549e47a83f039d3b0
netzrot <thorstenwschroeder@gmail.com>
2017-03-14 18:43:44 -0400

    handle api keys

--- loadResults.js
+++ loadResults.js
@@ -1,60 +1,8 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var superagent = require('superagent');
-var cheerio = require('cheerio');
 var request = require('request');
+var cheerio = require('cheerio');
 
-var loadStories = require('./loadStories');
-var loadResults = require('./loadResults');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-var stories = {
-	lastUpdated: 0, // 1970
-	left: [],
-	center: [],
-	right: []
-};
-
-var CacheTime = 5 * 60 * 1000;
-
-app.get('/', function(req, res) {
-	if (stories.lastUpdated < (new Date()) - CacheTime) {
-		
-		loadStories(process.env).
-		then(function(freshStories) {
-			stories = freshStories;
-			stories.lastUpdated = new Date();
-
-			res.render("index", { stories, maxStories: 6 }); // object of arrays
-		}).
-		catch(function(err) {
-			console.log(err);
-			res.status(500).render("error"); //send(err.toString());
-		});
-	} else {
-		res.render("index", { stories, maxStories: 6 }); // object of arrays
-	}
-});
-
-
-app.post('/search', function(req, res) {
-	var newSearch = req.body.search;
-
+module.exports = function loadResults(newSearch) {
+	
 	results = {
 		left: [],
 		center: [],
@@ -63,7 +11,7 @@ app.post('/search', function(req, res) {
 	request('http://www.huffingtonpost.com/search?keywords='+ newSearch + '&sortBy=recency&sortOrder=desc', function (error, response, html) {
 			
 		// if (error) {
-		// 	res.status(500).render("error');		
+		// 	res.redirect('/error');		
 		// };
 
 		if (!error && response.statusCode == 200) {
@@ -116,17 +64,9 @@ app.post('/search', function(req, res) {
 			var bodyObj = JSON.parse(body);
 								
 			results.right = bodyObj.response.docs;
-			res.render("results", { results });					    
-		};
+				return results;						    
+			};
 	});
 	});
 	});
-	
-});
-
-
-
-
-app.listen(3000, function() {
-	console.log("app started on port 3000");
-});
+};
--- loadStories.js
+++ loadStories.js
@@ -1,4 +1,4 @@
-var request = require('superagent');
+var superagent = require('superagent');
 
 module.exports = function loadStories(apiKeys) {
 	console.log('loading stories...');
@@ -10,44 +10,46 @@ module.exports = function loadStories(apiKeys) {
 	};
 
 	return new Promise(function(resolve, reject) {
-		request
-		    .get('https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey='+ apiKeys.NEWSAPI_KEY)
+		superagent
+		    .get('https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey=' + apiKeys.NEWSAPI_KEY)
 		    .end(function(err, response){
 
 		    	if (err) { return reject(err); }
 
 		   		stories.left = response.body.articles;
 
-			 	request
-				   .get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=' + apiKeys.NEWSAPI_KEY)
-				   .end(function(err, response){
+		superagent
+		   .get('https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=' + apiKeys.NEWSAPI_KEY)
+		   .end(function(err, response){
 
-				   		if (err) { return reject(err); }
+		   		if (err) { return reject(err); }
 
 				   		// response.body.articles.map(
 				   		// 	item => shortenDescription(item)
 				   		// )
 
-						for (var i = 0; i < response.body.articles.length; i++) {
-							response.body.articles[i].description = response.body.articles[i].description.substring(0,145)+"...";
-						}
+				for (var i = 0; i < response.body.articles.length; i++) {
+					response.body.articles[i].description = response.body.articles[i].description.substring(0,145)+"...";
+				}
 		   
-				   		stories.center = response.body.articles; // pushed array into object
+		   		stories.center = response.body.articles; // pushes array into object
 			   
 						   	
-					  	request
-						   .get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.foxnews.com%2Ffoxnews%2Fmost-popular&api_key=' + apiKeys.RSSJSON_KEY)
-						   .end(function(err, response){
+	 	superagent
+		   .get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.foxnews.com%2Ffoxnews%2Fmost-popular&api_key=' + apiKeys.RSSJSON_KEY)
+		   .end(function(err, response){
 
-						   		for (var i = 0; i < response.body.items.length; i++) {
-						   			response.body.items[i].description = response.body.items[i].description.split("<img")[0].substring(0,145)+"...";
-						   		}
+		   		console.log(response.body);
 
-						   		stories.right = response.body.items; // response.body.items is an array
+		   		for (var i = 0; i < response.body.items.length; i++) {
+		   			response.body.items[i].description = response.body.items[i].description.split("<img")[0].substring(0,145)+"...";
+		   		}
 
-						   		resolve(stories);
-					   	})
-				});  
+		   		stories.right = response.body.items; // response.body.items is an array
+
+		   		resolve(stories);
+	 	});
+		});  
 		});
 	});
 };
