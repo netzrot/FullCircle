7c4d9eb5e3ab6fc78ffecc58e4410b996e22cc2f
netzrot <thorstenwschroeder@gmail.com>
2017-03-27 22:12:01 -0400

    add favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var superagent = require('superagent');
@@ -17,7 +18,7 @@ app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5000)); // Heroku
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
